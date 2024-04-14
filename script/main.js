d09676df702e784f2c8bb42b327e5d46fcd1d623
Nutthapong Dissanont <tontooey@gmail.com>
2024-04-14 16:27:25 +0700

    feat: session storage

--- script/main.js
+++ script/main.js
@@ -1,24 +1,36 @@
 // Set pages contents
 let avatarPick = 0;
 let currentActiveDiv = null;
+let username = "Username";
 
 // game.html page
-export function setMiniLeaderboard(){
+ function setMiniLeaderboard(){
     // B.) FILL CODE
 }
 
-export function setUserBoard(){
+function setUserBoard(name){
     // C.) FILL CODE
-    let username = document.getElementById("username-input").textContent;
-    console.log(username)
+    // let username = document.getElementById("username-input").textContent;
+    // username = name;
+    sessionStorage.setItem('username', name);
+    console.log(name);
 }
 
-export function setAvatar(i){
+function getUserBoard(name){
+    // C.) FILL CODE
+    // let username = document.getElementById("username-input").textContent;
+    // username = name;
+    var name = sessionStorage.getItem('username');
+    return name;
+}
+
+
+ function setAvatar(i){
     avatarPick = i;
 
 }
 
-export function handleDivClick(event){
+ function handleDivClick(event){
     const ClickedDiv = event.target;
     if(currentActiveDiv !== null){
         currentActiveDiv.style.backgroundColor = '';
@@ -31,7 +43,7 @@ const container = document.getElementById("container");
 container.addEventListener('click', handleDivClick);
 
 // leaderboard.html page
-export function setLeaderboard(){
+ function setLeaderboard(){
     // B.) FILL CODE
 }
 

efe9baa829b94da1638da60f83c9381781d92d37
D12el <Delza.thanaphat@gmail.com>
2024-04-14 20:27:14 +0700

    game.html(avatar,username)

--- script/main.js
+++ script/main.js
@@ -16,22 +16,45 @@ function setUserBoard(name){
     console.log(name);
 }
 
-function getUserBoard(name){
+function getUserBoard(){
     // C.) FILL CODE
     // let username = document.getElementById("username-input").textContent;
     // username = name;
     var name = sessionStorage.getItem('username');
+    console.log(name)
     return name;
 }
 
+function setUserAvatarBoard(avatarnumber){
+    switch (avatarnumber){
+        case 1: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+        case 2: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+        case 3: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+        case 4: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+        case 5: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+        case 6: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+        case 7: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+        case 8: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+
+        default:
+            sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+            break;
+    }
+}
+
+function getUserAvatarBoard(){
+    var avatarImage = sessionStorage.getItem('avatarImage')
+    console.log(avatarImage);
+    return avatarImage;
+}
 
  function setAvatar(i){
     avatarPick = i;
-
+    setUserAvatarBoard(i);
+    console.log(sessionStorage.getItem('avatarImage'));
 }
 
- function handleDivClick(event){
-    const ClickedDiv = event.target;
+ function handleDivClick(ClickedDiv){
     if(currentActiveDiv !== null){
         currentActiveDiv.style.backgroundColor = '';
     }
@@ -39,8 +62,6 @@ function getUserBoard(name){
     currentActiveDiv.style.backgroundColor = 'red';
 }
 
-const container = document.getElementById("container");
-container.addEventListener('click', handleDivClick);
 
 // leaderboard.html page
  function setLeaderboard(){

3498909c5775594eefb4c068d469140fdd7a1418
D12el <Delza.thanaphat@gmail.com>
2024-04-14 15:24:32 +0700

    index.html

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 // Set pages contents
-
+let avatarPick = 0;
+let currentActiveDiv = null;
 
 // game.html page
 export function setMiniLeaderboard(){
@@ -8,7 +9,26 @@ export function setMiniLeaderboard(){
 
 export function setUserBoard(){
     // C.) FILL CODE
+    let username = document.getElementById("username-input").textContent;
+    console.log(username)
 }
+
+export function setAvatar(i){
+    avatarPick = i;
+
+}
+
+export function handleDivClick(event){
+    const ClickedDiv = event.target;
+    if(currentActiveDiv !== null){
+        currentActiveDiv.style.backgroundColor = '';
+    }
+    currentActiveDiv = ClickedDiv;
+    currentActiveDiv.style.backgroundColor = 'red';
+}
+
+const container = document.getElementById("container");
+container.addEventListener('click', handleDivClick);
 
 // leaderboard.html page
 export function setLeaderboard(){

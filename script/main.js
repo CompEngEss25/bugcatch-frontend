5c3992fb1c24788bc6e7da371d69fe8e80b06ebf
Nutthapong Dissanont <tontooey@gmail.com>
2024-04-15 18:14:08 +0700

    fix bugs

--- script/main.js
+++ script/main.js
@@ -1,65 +1,45 @@
 // Set pages contents
 let avatarPick = 0;
-let currentActiveDiv = null;
 let username = "Username";
+
+
 
 // game.html page
  function setMiniLeaderboard(){
     // B.) FILL CODE
 }
 
-function setUserBoard(name){
-    // C.) FILL CODE
-    // let username = document.getElementById("username-input").textContent;
-    // username = name;
-    sessionStorage.setItem('username', name);
-    console.log(name);
-}
-
-function getUserBoard(){
-    // C.) FILL CODE
-    // let username = document.getElementById("username-input").textContent;
-    // username = name;
+function getUsername(){
     var name = sessionStorage.getItem('username');
-    console.log(name)
     return name;
 }
 
-function setUserAvatarBoard(avatarnumber){
-    switch (avatarnumber){
-        case 1: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
-        case 2: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
-        case 3: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
-        case 4: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
-        case 5: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
-        case 6: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
-        case 7: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
-        case 8: sessionStorage.setItem('avatarImage', "images\bug-logo.png");
+function setUsername(name){
+    sessionStorage.setItem('username', name);
+}
 
+function getAvatarId(){
+    var avatarId = sessionStorage.getItem('avatarId');
+    return avatarId;
+}
+
+function setAvatarId(id){
+    sessionStorage.setItem('avatarId', id);
+}
+
+function getCurrentAvatarImg(){
+    switch (getAvatarId()){
+        case 1: return "images/bug-logo.png";
+        case 2: return "images/bug-logo.png";
+        case 3: return "images/bug-logo.png";
+        case 4: return "images/bug-logo.png";
+        case 5: return "images/bug-logo.png";
+        case 6: return "images/bug-logo.png";
+        case 7: return "images/bug-logo.png";
+        case 8: return "images/bug-logo.png";
         default:
-            sessionStorage.setItem('avatarImage', "images\bug-logo.png");
-            break;
+            return "images/bug-logo.png";
     }
-}
-
-function getUserAvatarBoard(){
-    var avatarImage = sessionStorage.getItem('avatarImage')
-    console.log(avatarImage);
-    return avatarImage;
-}
-
- function setAvatar(i){
-    avatarPick = i;
-    setUserAvatarBoard(i);
-    console.log(sessionStorage.getItem('avatarImage'));
-}
-
- function handleDivClick(ClickedDiv){
-    if(currentActiveDiv !== null){
-        currentActiveDiv.style.backgroundColor = '';
-    }
-    currentActiveDiv = ClickedDiv;
-    currentActiveDiv.style.backgroundColor = 'red';
 }
 
 

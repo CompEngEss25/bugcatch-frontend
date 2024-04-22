56c834b42bf16040a02771dc252523e326eb7ea6
Krit Hnoorun <krithnoorun@gmail.com>
2024-04-22 20:13:24 +0700

    add more difficulty

--- public/script/game.js
+++ public/script/game.js
@@ -47,8 +47,23 @@ let mode = 0; // 0; normal, 1; magnified, 2; x2, 3; moving, 4; shrink
 let tim = 119;
 let itemCount = 0;
 let fc = 0;
-let b1fc = 0;
-let b2fc = 0;
+let bugCount = 0;
+
+let antfc = 0;
+let beetlefc = 0;
+let cockroachfc = 0;
+let dragonflyfc = 0;
+let fatcfc = 0;
+let ladybugfc = 0;
+
+let bugScale = {
+    ant : 0.4,
+    beetle : 0.04,
+    cockroach : 0.04,
+    dragonfly : 0.04,
+    fatc : 0.04,
+    ladybug : 0.04
+}
 
 
 //game function===========================================================
@@ -67,6 +82,48 @@ function relocate(clickedBug) {
     clickedBug.angle = getRandomR();
 }
 
+//spawn bug
+function spawnBug(){
+    let a = Math.floor(Math.random()*6);
+    switch (a){
+        case 0:
+            relocate(gameScene.ant);
+            gameScene.ant.visible = true;
+            antfc = 0;
+            break;
+        case 1:
+            relocate(gameScene.beetle);
+            gameScene.beetle.visible = true;
+            beetlefc = 0;
+            break;
+        case 2:
+            relocate(gameScene.cockroach);
+            gameScene.cockroach.visible = true;
+            cockroachfc = 0;
+            break;
+        case 3:
+            relocate(gameScene.dragonfly);
+            gameScene.dragonfly.visible = true;
+            dragonflyfc = 0;
+            break;
+        case 4:
+            relocate(gameScene.fatc);
+            gameScene.fatc.visible = true;
+            fatcfc = 0;
+            break;
+        case 5:
+            relocate(gameScene.ladybug);
+            gameScene.ladybug.visible = true;
+            ladybugfc = 0;
+            break;
+        default :
+            relocate(gameScene.ant);
+            gameScene.ant.visible = true;
+            antfc = 0;
+    }
+    bugCount += 1;
+
+}
 
 //on click event
 function clickOnNormalBug(bug){
@@ -101,14 +158,18 @@ function clickOnBug(bug){
     }
 }
 function clickOnBg(){
-    score -= 20;
+    score -= 40;
 }
 function clickOnMagnify(){
     setItemInvisible();
     mode = 1;
     //perform effect
-    gameScene.bug1.setScale(0.2);
-    gameScene.bug2.setScale(0.2);
+    gameScene.ant.setScale(bugScale.ant*2);
+    gameScene.beetle.setScale(bugScale.beetle*2);
+    gameScene.cockroach.setScale(bugScale.cockroach*2);
+    gameScene.dragonfly.setScale(bugScale.dragonfly*2);
+    gameScene.fatc.setScale(bugScale.fatc*2);
+    gameScene.ladybug.setScale(bugScale.ladybug*2);
 
     //back to normal
     let t = setTimeout(backToNormal, 5000);
@@ -128,8 +189,13 @@ function clickOnShrink(){
     setItemInvisible();
     mode = 4;
     //perform effect
-    gameScene.bug1.setScale(0.05);
-    gameScene.bug2.setScale(0.05);
+    gameScene.ant.setScale(bugScale.ant/2);
+    gameScene.beetle.setScale(bugScale.beetle/2);
+    gameScene.cockroach.setScale(bugScale.cockroach/2);
+    gameScene.dragonfly.setScale(bugScale.dragonfly/2);
+    gameScene.fatc.setScale(bugScale.fatc/2);
+    gameScene.ladybug.setScale(bugScale.ladybug/2);
+
 
     //back to normal
     let t = setTimeout(backToNormal, 5000);
@@ -138,8 +204,13 @@ function clickOnShrink(){
 //function to get back to normal mode
 function backToNormal(){
     mode = 0;
-    gameScene.bug1.setScale(0.1);
-    gameScene.bug2.setScale(0.1);
+    gameScene.ant.setScale(bugScale.ant);
+    gameScene.beetle.setScale(bugScale.beetle);
+    gameScene.cockroach.setScale(bugScale.cockroach);
+    gameScene.dragonfly.setScale(bugScale.dragonfly);
+    gameScene.fatc.setScale(bugScale.fatc);
+    gameScene.ladybug.setScale(bugScale.ladybug);
+
 
 }
 
@@ -207,6 +278,43 @@ function setInvisible(it){
     itemCount -= 1;
 }
 
+//set bug invisible
+function setBugInvisible(bug){
+    bug.visible = false;
+    bugCount -= 1 ;
+    switch(bug){
+        case gameScene.ant:
+            antfc = 0;
+            break;
+        case gameScene.beetle:
+            beetlefc = 0;
+            break;
+        case gameScene.cockroach:
+            cockroachfc = 0;
+            break;
+        case gameScene.dragonfly:
+            dragonflyfc = 0;
+            break;
+        case gameScene.fatc:
+            fatcfc = 0;
+            break;
+        case gameScene.ladybug:
+            ladybugfc = 0;
+            break;
+        default:
+            break;
+    }
+}
+
+function setAllBugInvisible(){
+    gameScene.ant.visible = false;
+    gameScene.beetle.visible = false;
+    gameScene.cockroach.visible = false;
+    gameScene.dragonfly.visible = false;
+    gameScene.fatc.visible = false;
+    gameScene.ladybug.visible = false;
+}
+
 //check if the bug near edge
 function checkBugNearEdge(bug){
     if (bug.x < 10 || bug.x > sizes.width - 10 || bug.y < 10 || bug.y > sizes.height - 10){
@@ -234,12 +342,19 @@ function gameOver(){
 // Phaser==============================================================================
 
 function preload() {
+    //plain background for miss click
     this.load.image('bg', 'images/assets/transparent.png');
-    this.load.image('bug', 'images/assets/bug-logo.png');
-    this.load.image('bigIt', 'images/assets/B.png');
-    this.load.image('x2It', 'images/assets/X.png');
-    this.load.image('moveIt', 'images/assets/M.png');
-    this.load.image('shrinkIt', 'images/assets/S.png');
+
+    //items
+    this.load.image('mysBox', 'images/MysteryBox.png');
+
+    //bugs
+    this.load.image('ant', 'images/insects/ant.png');
+    this.load.image('beetle', 'images/insects/beetle.png');
+    this.load.image('cockroach', 'images/insects/cockroach.png');
+    this.load.image('dragonfly', 'images/insects/dragonfly.png');
+    this.load.image('fatc', 'images/insects/fatcockroach.png');
+    this.load.image('ladybug', 'images/insects/ladybug.png');
 }
 
 function create() {
@@ -258,31 +373,73 @@ function create() {
     
 
     //set the bug-----------------------------------------------------------------------
-    this.bug1 = this.add.sprite(getRandomX(), getRandomY(), 'bug');
-    this.bug1.setScale(0.1);
-    this.bug1.angle = getRandomR();
+    this.ant = this.add.sprite(getRandomX(), getRandomY(), 'ant');
+    this.ant.setScale(bugScale.ant);
+    this.ant.angle = getRandomR();
 
-    this.bug2 = this.add.sprite(getRandomX(), getRandomY(), 'bug');
-    this.bug2.setScale(0.1);
-    this.bug2.angle = getRandomR();
+    this.beetle = this.add.sprite(getRandomX(), getRandomY(), 'beetle');
+    this.beetle.setScale(bugScale.beetle);
+    this.beetle.angle = getRandomR();
+
+    this.cockroach = this.add.sprite(getRandomX(), getRandomY(), 'cockroach');
+    this.cockroach.setScale(bugScale.cockroach);
+    this.cockroach.angle = getRandomR();
+
+    this.dragonfly = this.add.sprite(getRandomX(), getRandomY(), 'dragonfly');
+    this.dragonfly.setScale(bugScale.dragonfly);
+    this.dragonfly.angle = getRandomR();
+
+    this.fatc = this.add.sprite(getRandomX(), getRandomY(), 'fatc');
+    this.fatc.setScale(bugScale.fatc);
+    this.fatc.angle = getRandomR();
+
+    this.ladybug = this.add.sprite(getRandomX(), getRandomY(), 'ladybug');
+    this.ladybug.setScale(bugScale.ladybug);
+    this.ladybug.angle = getRandomR();
+
 
     //set interactive
-    this.bug1.setInteractive();
-    this.bug1.on('pointerdown', function(){
-        console.log('bug1 clicked');
-        clickOnBug(gameScene.bug1);
+    this.ant.setInteractive();
+    this.ant.on('pointerdown', function(){
+        console.log('ant clicked');
+        clickOnBug(gameScene.ant);
         setScore();
-        b1fc = 0;
-    });
+    })
 
-    this.bug2.setInteractive();
-    this.bug2.on('pointerdown', function(){
-        console.log('bug2 clicked');
-        clickOnBug(gameScene.bug2);
+    this.beetle.setInteractive();
+    this.beetle.on('pointerdown', function(){
+        console.log('beetle clicked');
+        clickOnBug(gameScene.beetle);
         setScore();
-        b2fc = 0;
-    });
+    })
 
+    this.cockroach.setInteractive();
+    this.cockroach.on('pointerdown', function(){
+        console.log('cockroach clicked');
+        clickOnBug(gameScene.cockroach);
+        setScore();
+    })
+
+    this.dragonfly.setInteractive();
+    this.dragonfly.on('pointerdown', function(){
+        console.log('dragonfly clicked');
+        clickOnBug(gameScene.dragonfly);
+        setScore();
+    })
+
+    this.fatc.setInteractive();
+    this.fatc.on('pointerdown', function(){
+        console.log('fatcockroach clicked');
+        clickOnBug(gameScene.fatc);
+        setScore();
+    })
+
+    this.ladybug.setInteractive();
+    this.ladybug.on('pointerdown', function(){
+        console.log('ladybug clicked');
+        clickOnBug(gameScene.ladybug);
+        setScore();
+    })
 
     //set the score text----------------------------------------------------------------
     this.scoreTxt = this.add.text(0.7*sizes.width, 10, 'Score : 0', {
@@ -300,8 +457,8 @@ function create() {
 
     //set the item----------------------------------------------------------------------
     //big bug
-    this.bigItem = this.add.sprite(getRandomX(), getRandomY(), 'bigIt');
-    this.bigItem.setScale(0.2);
+    this.bigItem = this.add.sprite(getRandomX(), getRandomY(), 'mysBox');
+    this.bigItem.setScale(0.05);
 
     //set big bug interactive
     this.bigItem.setInteractive();
@@ -310,8 +467,8 @@ function create() {
     });
 
     //X2
-    this.x2Item = this.add.sprite(getRandomX(), getRandomY(), 'x2It');
-    this.x2Item.setScale(0.2);
+    this.x2Item = this.add.sprite(getRandomX(), getRandomY(), 'mysBox');
+    this.x2Item.setScale(0.05);
 
     //set X2 interactive
     this.x2Item.setInteractive();
@@ -320,8 +477,8 @@ function create() {
     });
 
     //move
-    this.moveItem = this.add.sprite(getRandomX(), getRandomY(), 'moveIt');
-    this.moveItem.setScale(0.2);
+    this.moveItem = this.add.sprite(getRandomX(), getRandomY(), 'mysBox');
+    this.moveItem.setScale(0.05);
 
     //set move interactive
     this.moveItem.setInteractive();
@@ -330,8 +487,8 @@ function create() {
     });
 
     //shrink
-    this.shrinkItem = this.add.sprite(getRandomX(), getRandomY(), 'shrinkIt');
-    this.shrinkItem.setScale(0.2);
+    this.shrinkItem = this.add.sprite(getRandomX(), getRandomY(), 'mysBox');
+    this.shrinkItem.setScale(0.05);
 
     //set shrink interactive
     this.shrinkItem.setInteractive();
@@ -341,7 +498,9 @@ function create() {
 
     //
     setItemInvisible();
-
+    setAllBugInvisible();
+    spawnBug();
+    spawnBug();
 
 }
 
@@ -349,13 +508,22 @@ function create() {
 
 function update() {
     //for moving item
-    checkBugNearEdge(gameScene.bug1);
-    checkBugNearEdge(gameScene.bug2);
+    checkBugNearEdge(gameScene.ant);
+    checkBugNearEdge(gameScene.beetle);
+    checkBugNearEdge(gameScene.cockroach);
+    checkBugNearEdge(gameScene.dragonfly);
+    checkBugNearEdge(gameScene.fatc);
+    checkBugNearEdge(gameScene.ladybug);
 
     let speed = 2;
     if (mode === 3){
-        gameScene.bug1.setPosition(gameScene.bug1.x + (Math.cos((gameScene.bug1.angle-90)*Math.PI/180)*speed), gameScene.bug1.y + (Math.sin((gameScene.bug1.angle-90)*Math.PI/180)*speed))
-        gameScene.bug2.setPosition(gameScene.bug2.x + (Math.cos((gameScene.bug2.angle-90)*Math.PI/180)*speed), gameScene.bug2.y + (Math.sin((gameScene.bug2.angle-90)*Math.PI/180)*speed))
+        gameScene.ant.setPosition(gameScene.ant.x + (Math.cos((gameScene.ant.angle-90)*Math.PI/180)*speed), gameScene.ant.y + (Math.sin((gameScene.ant.angle-90)*Math.PI/180)*speed))
+        gameScene.beetle.setPosition(gameScene.beetle.x + (Math.cos((gameScene.beetle.angle-90)*Math.PI/180)*speed), gameScene.beetle.y + (Math.sin((gameScene.beetle.angle-90)*Math.PI/180)*speed))
+        gameScene.cockroach.setPosition(gameScene.cockroach.x + (Math.cos((gameScene.cockroach.angle-90)*Math.PI/180)*speed), gameScene.cockroach.y + (Math.sin((gameScene.cockroach.angle-90)*Math.PI/180)*speed))
+        gameScene.dragonfly.setPosition(gameScene.dragonfly.x + (Math.cos((gameScene.dragonfly.angle-90)*Math.PI/180)*speed), gameScene.dragonfly.y + (Math.sin((gameScene.dragonfly.angle-90)*Math.PI/180)*speed))
+        gameScene.fatc.setPosition(gameScene.fatc.x + (Math.cos((gameScene.fatc.angle-90)*Math.PI/180)*speed), gameScene.fatc.y + (Math.sin((gameScene.fatc.angle-90)*Math.PI/180)*speed))
+        gameScene.ladybug.setPosition(gameScene.ladybug.x + (Math.cos((gameScene.ladybug.angle-90)*Math.PI/180)*speed), gameScene.ladybug.y + (Math.sin((gameScene.ladybug.angle-90)*Math.PI/180)*speed))
+    
     }
     
     //for spawn item
@@ -371,32 +539,71 @@ function update() {
     }
 
     //higher score, bug stay less time
-    if (mode === 0){
+    if (mode !== 3){
+        let bc = 0;
         let t = 300;
-        if (score > 4000) t = 180;
+        if (score > 8000) t = 60;
+        else if (score > 7000) t = 90;
+        else if (score > 6000) t = 120;
+        else if (score > 5000) t = 150;
+        else if (score > 4000) t = 180;
         else if (score > 3000) t = 210;
         else if (score > 2000) t = 240;
         else if (score > 1000) t = 270;
 
-        if (b1fc > t) {
-            relocate(gameScene.bug1);
-            b1fc = 0;
+        if (gameScene.ant.visible){
+            antfc += 1;
+            if (antfc > t){
+                setBugInvisible(gameScene.ant);
+                spawnBug();
+            }
+            bc += 1;
         }
-        if (b2fc > t) {
-            relocate(gameScene.bug2);
-            b2fc = 0;
+        if (gameScene.beetle.visible){
+            beetlefc += 1;
+            if (beetlefc > t){
+                setBugInvisible(gameScene.beetle);
+                spawnBug();
+            }
+            bc += 1;
+        }
+        if (gameScene.cockroach.visible){
+            cockroachfc += 1;
+            if (cockroachfc > t){
+                setBugInvisible(gameScene.cockroach);
+                spawnBug();
+            }
+            bc += 1;
+        }
+        if (gameScene.dragonfly.visible){
+            dragonflyfc += 1;
+            if (dragonflyfc > t){
+                setBugInvisible(gameScene.dragonfly);
+                spawnBug();
+            }
+            bc += 1;
+        }
+        if (gameScene.fatc.visible){
+            fatcfc += 1;
+            if (fatcfc > t){
+                setBugInvisible(gameScene.fatc);
+                spawnBug();
+            }
+            bc += 1;
+        }
+        if (gameScene.ladybug.visible){
+            ladybugfc += 1;
+            if (ladybugfc > t){
+                setBugInvisible(gameScene.ladybug);
+                spawnBug();
+            }
+            bc += 1;
         }
 
-        b1fc += 1;
-        b2fc += 1;
+        bugCount = bc;
     }
+    if (bugCount < 2) spawnBug();
 
-    //for responsive
-    /*(function() {
-        const gameId = document.getElementById("gameContainer"); // Target div that wraps the phaser game
-        gameId.style.width = '100%'; // set width to 100%
-        gameId.style.height = '100%'; // set height to 100%
-    })(); // run function*/
 }
 
 
